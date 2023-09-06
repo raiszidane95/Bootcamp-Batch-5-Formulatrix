cfbf369d9b0d0d9dc0cd5c11cc9614409071e4a3
raiszidane95 <raiszidane95@gmail.com>
2023-09-06 11:57:40 +0700

    Upcast Downcast Explicit Implicit

---
+++ project9/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        //Upcast = small size type -> big size type
+        //Implicit
+        int x = 9; //4 byte / 32 bit
+        double y = x; //8 byte
+
+        //Downcast = big size type -> small size type
+        //Explicit
+        double z = 10000000000;
+        int q = (int)z; //Explicit cast
+
+        Console.WriteLine(x);
+        Console.WriteLine(q);
+    }
+}
