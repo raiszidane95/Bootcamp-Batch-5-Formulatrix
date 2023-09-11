2ce0c72c68d358e2f545df848fa75ffbf58e6689
raiszidane95 <raiszidane95@gmail.com>
2023-09-11 11:57:53 +0700

    Is Type Cheking

---
+++ IsType/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    object x = 32;
+    public static void Main()
+    {
+
+    }
+}
+
+public static class MyExtensionMethod
+{
+    public static void CheckType(this object x)
+    {
+        if (x is int)
+        {
+            Console.WriteLine($"this is Int Type");
+        }
+        if (x is ValueType)
+        {
+            Console.WriteLine("This is ValueType");
+        }
+    }
+}
