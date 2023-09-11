8f04b64501f6a65e75779e39c4229d5160c7914c
raiszidane95 <raiszidane95@gmail.com>
2023-09-11 11:55:15 +0700

    Var type

---
+++ Var/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+
+    public static void Main()
+    {
+        var x = new Car(); //var => Car // var is defined by Car class
+                           //x = 3; // 3 is not Car
+
+        Console.WriteLine($"{x.name}");
+
+        var number = 1;
+        double number2 = number;
+        Console.WriteLine(number2);
+        ;
+    }
+}
+
+public class Car
+{
+    public string name = "Honda";
+}
+
