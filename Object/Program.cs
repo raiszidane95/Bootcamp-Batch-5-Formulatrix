80b6b80b89b600d0026e0a44958a1d1536b339fc
raiszidane95 <raiszidane95@gmail.com>
2023-09-11 11:57:21 +0700

    Object type

---
+++ Object/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int x = 32;
+        object y = x; //boxing
+
+        int z = (int)y; //unboxing
+
+        // int f = 33;
+        // object g = f;
+        // long l = (int)g;
+        Console.WriteLine(z);
+
+        // int angka = 100;
+        object angka1 = 123;
+        long angka3 = (int)angka1; // Must cast with explixcit casting
+        Console.WriteLine(angka3);
+    }
+}
