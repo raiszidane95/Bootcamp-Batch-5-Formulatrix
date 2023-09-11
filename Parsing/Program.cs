c898ed447dfe0a32fa48d055b71fba7475b70abf
raiszidane95 <raiszidane95@gmail.com>
2023-09-11 11:57:03 +0700

    Parsing type

---
+++ Parsing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+public class Program
+{
+    public static void Main()
+    {
+        string number = "20i0";
+        int result;
+
+        bool isParseAble = int.TryParse(number, out result);
+        Console.WriteLine(isParseAble);
+        Console.WriteLine(result);
+
+        int reusltInt = int.Parse(number);
+        Console.WriteLine(reusltInt);
+    }
+
+}
